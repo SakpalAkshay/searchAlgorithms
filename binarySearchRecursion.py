d5625fcfb3ea5e600ed560fb4c10a075e30136af
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-08-06 09:58:27 +0530

    Added list input

--- binarySearchRecursion.py
+++ binarySearchRecursion.py
@@ -1,10 +1,8 @@
 def binarySearch(a,x,si,ei):
     # list should be sorted
-
     #base case if startIndex si crosses end index ei
     if(si > ei):
         return -1
-    
     mid = (si + ei)//2
     if a[mid] == x:
         return mid
@@ -12,3 +10,11 @@ def binarySearch(a,x,si,ei):
         return binarySearch(a,x,si, mid - 1)
     else:
         return binarySearch(a,x,mid + 1,si)
+
+li = [int(x) for x in input.split()]
+print("Given List: ",li)
+li.sort()
+print("Sorted list is :", li)
+element = int(input("enter the element you want to search: "))
+index = binarySearch(li, element,0,len(li)-1)
+print(index)
